3d084a43dbc5e76b6e4ce21dbefdf269ebc8d442
Gonzalo Sakuda <gonchissn@gmail.com>
2023-02-04 05:48:11 +0100

    Add datasource auto configuration

--- src/main/java/com/promo88/Main.java
+++ src/main/java/com/promo88/Main.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class Main {
     public static void main(String[] args) {
