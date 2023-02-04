671ecb66a268909780b20bd72c58c56324a4f6ec
Gonzalo Sakuda <gonchissn@gmail.com>
2023-02-04 06:16:57 +0100

    Added JPA and HIbernate propperties

--- src/main/java/com/promo88/Main.java
+++ src/main/java/com/promo88/Main.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
